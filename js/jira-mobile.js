b1e11bf7f5b1785bf8d8d57843d133dcec65d080
Vladimir Vasilyev <djsuprin@gmail.com>
2014-07-06 16:51:54 +0200

    - jQueryMobile updated to 1.4.3 (older versions were removed); - created one external menu panel and removed all duplicated panels; - index.html: moved scripts on top to get rid of flash of unstyled content; - added summary to issue page; - replaced page event handlers with the appropriate page container events; - added loading widgets to pages when ajax requests are made there; - implemented wiki2html function to convert issue description and comments to html; - many other stuff which I don't remember because don't follow good commit practices. Stupid.

--- js/jira-mobile.js
+++ js/jira-mobile.js
@@ -3,7 +3,7 @@
     (function () {
 
         var DO_PROXY            = true,
-            SECONDS_IN_WORKDAY  = 28800; // 8 hours workday
+            SECONDS_IN_WORKDAY  = 28800, // 8 hours workday
             // REST API links
             PROJECTS_LINK       = '/rest/api/latest/project',
             FILTERS_LINK        = '/rest/api/latest/filter/favourite',
@@ -13,23 +13,60 @@
             jiraLink            = localStorage.getItem('jiraLink'),
             authHeaderValue     = localStorage.getItem('authHeaderValue'),
             // Variables containing current state of app components
+            selectedFilter = {
+                'filterName': '',
+                'filterJQL': ''
+            },
             currentIssueKey     = '';
 
-        $(document).on("pagecreate", '#dashboard', function () {
-            if (jiraLink == null || jiraLink == '' || authHeaderValue == null || authHeaderValue == '') {
-                $( "body" ).pagecontainer( "change", "#settings", { transition: "slide" });
+        $(function() {
+            // execute when page is loaded
+
+            $('#save-settings-button').tap(saveSettings);
+
+            $('#clear-credentials-button').tap(function (e) {
+                e.preventDefault();
+                localStorage.clear();
+            });
+
+            $('#filter-button').tap(function (e) {
+                e.preventDefault();
+                filterIssues();
+            });
+
+            $( "#menu_panel" ).panel();
+            $( "#menu-list" ).listview().enhanceWithin();
+            $.mobile.defaultPageTransition = 'slide';
+        });
+
+        /*$(document).on("swiperight", '[data-role="page"]', function(e) {
+            $( "#menu_panel" ).panel( "open" );
+        });*/
+
+        $(document).on( "pagecontainershow", function(e, ui) {
+            var pageId = ui.toPage.attr("id");
+            switch (pageId) {
+                case "dashboard": showDashboard(); break;
+                case "settings": loadSettings(); break;
+                case "projects": showProjects(); break;
+                case "filters": showFilters(); break;
+                case "issues": filterIssues(); break;
+                case "issue": showIssue(); break;
             }
         });
 
-        $(document).on('pagecreate', '#settings', loadSettings);
+        function showDashboard() {
+            if (jiraLink == null || jiraLink == '' || authHeaderValue == null || authHeaderValue == '') {
+                $( "body" ).pagecontainer( "change", "#settings");
+            }
+        }
 
-        $(document).on('pagecreate', '#projects', function (e) {
-
+        function showProjects() {
+            $.mobile.loading( "show" );
             $.ajax({
                 type: "GET",
                 url: getUrl(jiraLink + PROJECTS_LINK),
                 dataType: 'json',
-                async: false,
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader('Authorization', authHeaderValue);
                 },
@@ -48,22 +85,22 @@
                         $list.append($('<li/>').html($a));
                     }
                     $list.listview('refresh');
+                    $.mobile.loading( "hide" );
                 },
                 error: function (data) {
-                    console.log('Error');
+                    console.log('Error while retrieving projects.');
                     console.log(data);
+                    $.mobile.loading( "hide" );
                 }
             });
-        });
+        }
 
-
-        $(document).on('pagecreate', '#filters', function (e) {
-
+        function showFilters() {
+            $.mobile.loading( "show" );
             $.ajax({
                 type: "GET",
                 url: getUrl(jiraLink + FILTERS_LINK),
                 dataType: 'json',
-                async: false,
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader('Authorization', authHeaderValue);
                 },
@@ -84,38 +121,36 @@
                                 href: '#'
                             }).html(filterName).tap(function (e) {
                                 e.preventDefault();
-                                $('#filter-name').find('.ui-btn-text').html(filterName)
-                                $('#jql-textarea').html(filterJQL);
-                                //$.mobile.changePage('#issues');
-                                $( "body" ).pagecontainer( "change", "#issues", { transition: "slide" });
+                                //$('#filter-name').find('.ui-btn-text').html(filterName)
+                                //$('#jql-textarea').html(filterJQL);
+                                selectedFilter.filterName = filterName;
+                                selectedFilter.filterJQL = filterJQL;
+                                $( "body" ).pagecontainer( "change", "#issues");
+                                
                             });
                             $list.append($('<li/>').html($a));
                         }(filterName, filterJQL);
 
                     }
                     $list.listview('refresh');
+                    $.mobile.loading( "hide" );
                 },
                 error: function (data) {
-                    console.log('Error');
+                    console.log('Error while retrieving filters.');
                     console.log(data);
+                    $.mobile.loading( "hide" );
                 }
             });
-        });
+        }
 
-
-        $(document).on('pagecreate', '#issues', function (e) {
-
-            filterIssues();
-        });
-
-
-        $(document).on('pagecreate', '#issue', function (e) {
+        function showIssue() {
             $('#issue').find('#issue-page-title').html(currentIssueKey);
+            $('#issue-details-collapsible').collapsible("expand");
+            $.mobile.loading( "show" );
             $.ajax({
                 type: "GET",
                 url: getUrl(jiraLink + ISSUE_LINK + currentIssueKey),
                 dataType: 'json',
-                async: false,
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader('Authorization', authHeaderValue);
                 },
@@ -123,8 +158,6 @@
                     if (DO_PROXY) {
                         data = data.contents;
                     }
-                    console.log("Issue:");
-                    console.log(data);
                     var issueFields = data['fields'];
                     var issue = {
                         description: issueFields['description'] !== null ? issueFields['description'] : 'No description',
@@ -134,6 +167,7 @@
                         duedate: new Date(issueFields['duedate']).toLocaleString()
                     }
 
+                    $('#issue-summary').html(issueFields['summary']);
                     $('#issue-type').html(issueFields['issuetype']['name']);
                     $('#issue-status').html(issueFields['status']['name']);
                     $('#issue-resolution').html(issue['resolution']);
@@ -141,57 +175,58 @@
                     $('#issue-assignee').html(issueFields['assignee']['displayName']);
                     $('#issue-reporter').html(issueFields['reporter']['displayName']);
 
-            createButtonsFromArray(issueFields['versions'], '#issue-affects-versions');
-            createButtonsFromArray(issueFields['fixVersions'], '#issue-fix-versions');
-            createButtonsFromArray(issueFields['components'], '#issue-components');
-            createButtonsFromArray(issueFields['labels'], '#issue-labels');
-            
-                    $('#issue-description').html(issue['description']);
+                    createButtonsFromArray(issueFields['versions'], '#issue-affects-versions');
+                    createButtonsFromArray(issueFields['fixVersions'], '#issue-fix-versions');
+                    createButtonsFromArray(issueFields['components'], '#issue-components');
+                    createButtonsFromArray(issueFields['labels'], '#issue-labels');
+
+                    $('#issue-description').html(wiki2html(issue['description']));
                     $('#issue-created').html(issue['created']);
                     $('#issue-updated').html(issue['updated']);
                     $('#issue-due-date').html(issue['duedate']);
                     $('#issue-estimated').html(issueFields['timetracking']['originalEstimate']);
                     $('#issue-remaining').html(issueFields['timetracking']['remainingEstimate']);
                     $('#issue-logged').html(issueFields['timetracking']['timeSpent']);
+                    $.mobile.loading( "hide" );
                 },
                 error: function (data) {
-                    console.log('Error');
+                    console.log('Error while retrieving issue info.');
                     console.log(data);
+                    $.mobile.loading( "hide" );
                 }
             });
-        });
+        }
         
         function createButtonsFromArray(elements, placeholderSelector) {
-        if (elements !== null && elements.length > 0) {
-            var $div = $('<div/>');
-            for (var i = 0; i < elements.length; i++) {
-            var name = (typeof elements[i] == 'string' || elements[i] instanceof String) ? elements[i] : elements[i]['name'];
-            var $button = $('<a/>').attr({
-                'data-role': 'button',
-                'data-inline': true,
-                'data-mini': true,
-                'href': '#'
-            }).html(name).tap(function (e) {
-                e.preventDefault();
-            }).taphold(function(e) {
-                console.log("edit");
-                e.preventDefault();
-            }).addClass('ui-btn ui-btn-inline ui-mini');
-            $div.append($button);
+            if (typeof elements !== 'undefined' && elements !== null && elements.length > 0) {
+                var $div = $('<div/>');
+                for (var i = 0; i < elements.length; i++) {
+                var name = (typeof elements[i] == 'string' || elements[i] instanceof String) ? elements[i] : elements[i]['name'];
+                var $button = $('<a/>').attr({
+                    'data-role': 'button',
+                    'data-inline': true,
+                    'data-mini': true,
+                    'href': '#'
+                }).html(name).tap(function (e) {
+                    e.preventDefault();
+                }).taphold(function(e) {
+                    console.log("edit");
+                    e.preventDefault();
+                }).addClass('ui-btn ui-btn-inline ui-mini');
+                $div.append($button);
+                }
+                $(placeholderSelector).html($div);
+            } else {
+                $(placeholderSelector).html('None');
             }
-            $(placeholderSelector).html($div);
-        } else {
-            $(placeholderSelector).html('None');
         }
-        
-        
-            
-        }
-
 
         function filterIssues() {
             var $table = $('#issues-table');
             var $tableBody = $table.find('tbody');
+            $('#filter-name-field').val(selectedFilter.filterName)
+            $('#jql-textarea').html(selectedFilter.filterJQL);
+            $('#filter-collapsible').collapsible("expand");
             // escaping JQL twice because PHP proxy script unescapes GET params
             var jql = escape($('#jql-textarea').val());
             if (jql.trim() != '') {
@@ -199,8 +234,6 @@
                     type: "GET",
                     url: getUrl(jiraLink + ISSUES_LINK + jql),
                     dataType: 'json',
-                    async: false,
-                    //data: '{}',
                     beforeSend: function (xhr) {
                         xhr.setRequestHeader('Authorization', authHeaderValue);
                     },
@@ -208,6 +241,7 @@
                         if (DO_PROXY) {
                             data = data.contents
                         }
+                        var templateData = { issues : [] };
                         $tableBody.html('');
                         var issues = data['issues'];
                         for (var i = 0; i < issues.length; i++) {
@@ -218,34 +252,24 @@
                                 'priority': issues[i]['fields']['priority'] == null ? null : issues[i]['fields']['priority']['name'],
                                 'status': issues[i]['fields']['status'] == null ? null : issues[i]['fields']['status']['name']
                             };
-                            var closure = function (issue, $tableBody) {
-                                var $key = $('<a/>').attr({
-                                    'href': '#',
-                                }).tap(function (e) {
+                            templateData.issues.push(issue);
+                        }
+                        var tableBodyHtml = Mustache.to_html($('#issues-table-rows-tpl').html(), templateData);
+                        $tableBody.html(tableBodyHtml);
+                        for (var i = 0; i < templateData.issues.length; i++) {
+                            (function (issueKey) {
+                                var row = $("#issues-table > tbody > tr").get(i);
+                                $(row).find("a").tap(function (e) {
                                     e.preventDefault();
-                                    currentIssueKey = issue['key'];
-                                    $( "body" ).pagecontainer( "change", "#issue", { transition: "slide" });
-                                }).html(issue['key']);
-                                var $summary = $('<a/>').attr({
-                                    'href': '#',
-                                }).tap(function (e) {
-                                    e.preventDefault();
-                                    currentIssueKey = issue['key'];
-                                    $( "body" ).pagecontainer( "change", "#issue", { transition: "slide" });
-                                }).html(issue['summary']);
-                                var $tr = $('<tr/>')
-                                    .append($('<td/>').html(issue['type']))
-                                    .append($('<td/>').html($key))
-                                    .append($('<td/>').html($summary))
-                                    .append($('<td/>').html(issue['priority']))
-                                    .append($('<td/>').html(issue['status']));
-                                $tableBody.append($tr);
-                            }(issue, $tableBody);
+                                    currentIssueKey = issueKey;
+                                    $( "body" ).pagecontainer( "change", "#issue" );
+                                });
+                            })(templateData.issues[i].key);
                         }
                         $table.table('refresh');
                     },
                     error: function (data) {
-                        console.log('Error');
+                        console.log('Error while retrieving issues.');
                         console.log(data);
                     }
                 });
@@ -253,24 +277,6 @@
                 $tableBody.html('');
             }
         }
-
-
-        $(document).ready(function () {
-            // execute when page is loaded
-
-            $('#save-settings-button').tap(saveSettings);
-
-            $('#clear-credentials-button').tap(function (e) {
-                e.preventDefault();
-                localStorage.clear();
-            });
-
-            $('#filter-button').tap(function (e) {
-                e.preventDefault();
-                filterIssues();
-            });
-
-        });
 
         function getUrl(url) {
             if (DO_PROXY) {
@@ -290,7 +296,7 @@
         localStorage.setItem("username", username);
         localStorage.setItem("password", password);
         localStorage.setItem("authHeaderValue", authHeaderValue);
-        $( "body" ).pagecontainer( "change", "#projects", { transition: "slide" });
+        $( "body" ).pagecontainer( "change", "#projects");
         }
 
         function loadSettings(e) {
@@ -303,6 +309,18 @@
             var tok = username + ':' + password;
             var hash = btoa(tok);
             return "Basic " + hash;
+        }
+
+        // TODO: need to convert wiki content (issue description and comments) to html
+        function wiki2html(wiki) {
+            // replace line endings
+            return wiki.replace(/\n/g, "<br />")
+                .replace(/\[(.*)|(html.*)\]/g, function(str) {
+                    var temp = str.split('|');
+                    var text = temp[0].substring(1);
+                    var href = temp[1].substring(0, temp[1].lastIndexOf(']'));
+                    return '<a href="' + href + '">' + text + '</a>';
+                });
         }
         
         /*function getDaysAndWeeksByTimestamp(timestamp) {
